07d715d51deaa466ddb62867cf1dd55a9fac7929
iam24400 <mendozavalde1095@gmail.com>
2025-05-07 21:28:51 +0800

    updated backend

--- backend/app.js
+++ backend/app.js
@@ -4,17 +4,19 @@ import cors from "cors";
 import appRouter from "./router/appRouter.js"
 import authRouter from "./router/authRouter.js"
 import logger from './middleware/logger.js';
+import cronJob from './cron/cron.js';
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+cronJob.start();
 app.use(express.json());
 app.use(cors());
 app.use(logger);
 
 app.use("/api/auth", authRouter);
 app.use("/api/byaHero", appRouter);
+
 
 
 app.listen(3000, () => {
