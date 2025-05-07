f6ec6dbf0ad20d3576809ec7af7cf6588d06b5a7
iam24400 <mendozavalde1095@gmail.com>
2025-05-07 21:35:36 +0800

    updated backend

--- backend/cron/cron.js
+++ backend/cron/cron.js
@@ -7,7 +7,7 @@ const job = new cron.CronJob("*/1 * * * *", function () {
       if (res.statusCode === 200) console.log("GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
-    .on("error", (e) => console.error("Error while sending request", e));
+    .on("error", (err) => console.error("Error while sending request", err));
 });
 
 export default job;
